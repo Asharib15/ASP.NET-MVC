a15330a798b974eb792d188f2366470cb9363a1a
Muhammad Asharib <132045877+Asharib15@users.noreply.github.com>
2024-03-22 00:26:24 +0500

    Initial commit

---
+++ ASHARIB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASHARIB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
